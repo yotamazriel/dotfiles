b1252842a81b9a1d73b9484cd71b38dd5394ee14
yotamazriel <yotam@tensorleap.ai>
2020-09-06 09:50:50 +0300

    Fix symlinks

--- bootstrap/symlinks.sh
+++ bootstrap/symlinks.sh
@@ -1,7 +1,6 @@
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/nvim ~/.config/nvim
-# ln -s ~/.dotfiles/.mongorc.js ~/.mongorc.js
-ln -s ~/.dotfiles/.zshenv ~/.zshenv
+ln -s ~/.dotfiles/zsh/.zshenv ~/.zshenv
 ln -s ~/.dotfiles/.editorconfig ~/.editorconfig
 
