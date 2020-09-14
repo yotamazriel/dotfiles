43693991c5145a869fdab608a83d285946bf1d5c
yotamazriel <yotam@tensorleap.ai>
2020-09-14 15:53:14 +0300

    Install carvel

--- bootstrap/devtools.sh
+++ bootstrap/devtools.sh
@@ -17,6 +17,7 @@ brew cask install docker
 brew cask install google-cloud-sdk
 brew install the_silver_searcher
 brew install ripgrep
-
+wget -O- https://k14s.io/install.sh | bash
+curl -L https://k14s.io/install.sh | bash
 # install nvm and latest node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
