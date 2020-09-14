cf823b1f8208afa9d237a7b6295e0e9f22f40129
yotamazriel <yotam@tensorleap.ai>
2020-09-14 16:08:32 +0300

    Install octant

--- bootstrap/devtools.sh
+++ bootstrap/devtools.sh
@@ -19,5 +19,6 @@ brew install the_silver_searcher
 brew install ripgrep
 wget -O- https://k14s.io/install.sh | bash
 curl -L https://k14s.io/install.sh | bash
+brew install octant
 # install nvm and latest node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
