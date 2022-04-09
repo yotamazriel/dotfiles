6ef4ab8b08bbde6cf3d03744509f9541048f366f
yotamazriel <yotam@tensorleap.ai>
2022-04-09 11:14:16 +0300

    Update bootstrap

--- bootstrap/devtools.sh
+++ bootstrap/devtools.sh
@@ -7,19 +7,21 @@ brew install z
 brew install wget
 brew install antigen
 brew install direnv
-brew install hub
 brew install jq
 brew install htop
 brew install fzf
 brew install neovim
 /usr/local/opt/fzf/install
 brew install pyenv
-brew cask install docker
-brew cask install google-cloud-sdk
+brew install docker --cask
+brew install google-cloud-sdk --cask
+brew install rectangle --cask
 brew install the_silver_searcher
 brew install ripgrep
+brew install kubectl
 wget -O- https://k14s.io/install.sh | bash
 curl -L https://k14s.io/install.sh | bash
 brew install octant
 # install nvm and latest node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+brew install gh
