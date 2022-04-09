784f91bbe690d796cc864d1af51df60f8cbebf26
yotamazriel <yotam@tensorleap.ai>
2022-04-09 11:14:16 +0300

    Update bootstrap

--- bootstrap/apps.sh
+++ bootstrap/apps.sh
@@ -8,19 +8,14 @@ rm -rf fonts
 
 # install iterm
 ITERM_SYNC_DIR="${HOME}/.dotfiles/iterm"
-brew cask install iterm2
+brew install iterm2 --cask
 defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool TRUE
 defaults write com.googlecode.iterm2 PrefsCustomFolder $ITERM_SYNC_DIR
 
-brew cask install google-chrome
-brew cask install dropbox
-brew cask install google-drive
-brew cask install java
-brew cask install the-unarchiver
-brew cask install whatsapp
-brew cask install karabiner-elements
-brew cask install vlc
-brew cask install appcleaner
-brew cask install slack
-brew cask install visual-studio-code
-brew cask install zoomus
+brew install google-chrome --cask
+brew install java --cask
+brew install whatsapp --cask
+brew install slack --cask
+brew install visual-studio-code --cask
+brew install zoom --cask
+brew install karabiner-elements --cask
